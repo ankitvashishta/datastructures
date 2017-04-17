517c605123db5ef25429830bf1b5a913461fbe02
Ankit Vashishta <ankitvashishta@gmail.com>
2017-04-17 11:04:54 +0530

    Bubble sort

--- DataStructures/src/com/ankit/data/structures/sorting/BubbleSort.java
+++ DataStructures/src/com/ankit/data/structures/sorting/BubbleSort.java
@@ -6,12 +6,18 @@ public class BubbleSort {
 
 	public static void main(String args[]) {
 		Integer[] arr = { 5, 2, 4, 3, 6, 1, 99, 101, 88, 67, 34, 2, 4, 99, -1 };
+		Integer[] arr1 = { 5, 2, 4, 3, 6, 1, 99, 101, 88, 67, 34, 2, 4, 99, -1 };
+		System.out.println(arr.length);
 		BubbleSort bubbleSort = new BubbleSort();
 		bubbleSort.bubbleSort(arr);
 		for (Integer i : arr) {
 			System.out.print(i + " , ");
 		}
-		//System.out.println("\n" + counter);
+		System.out.println();
+		bubbleSort.bubbleSort_Incremental(arr1);
+		for (Integer i : arr1) {
+			System.out.print(i + " , ");
+		}
 	}
 
 	private void bubbleSort(Integer arr[]) {
@@ -26,10 +32,24 @@ public class BubbleSort {
 			}
 		}
 	}
+
+	private void bubbleSort_Incremental(Integer arr[]) {
+		int arrayLengthLimit = arr.length - 1;
+		for (int i = 0; i < arrayLengthLimit; i++) {
+			for (int j = 0; j < arrayLengthLimit - i; j++) {
+				counter++;
+				int key = arr[j];
+				if (arr[j] > arr[j + 1]) {
+					arr[j] = arr[j + 1];
+					arr[j + 1] = key;
+				}
+			}
+		}
+	}
+
 }
-//Check the first step of Bubble Sort
-/*int key = arr[i];
-if (arr[i] > arr[i + 1]) {
-	arr[i] = arr[i + 1];
-	arr[i + 1] = key;
-}*/
+// Check the first step of Bubble Sort
+/*
+ * int key = arr[i]; if (arr[i] > arr[i + 1]) { arr[i] = arr[i + 1]; arr[i + 1]
+ * = key; }
+ */
