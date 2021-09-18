f3ea20e3bada1b682830304b84cae23d02e28676
Ankit Vashishta <ankitvashishta@gmail.com>
2021-09-18 22:03:33 +0800

    String : Balanced Braces

---
+++ DataStructures/src/com/ankit/data/structures/strings/BalancedBraces.java
@@ -0,0 +1,62 @@
+package com.ankit.data.structures.strings;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * For a given number ‘N’, write a function to generate all combination of ‘N’
+ * pairs of balanced parentheses.
+ * 
+ * Example -> Input: N=3
+ * 
+ * Output: ((())), (()()), (())(), ()(()), ()()()
+ * 
+ * @author ankit
+ *
+ */
+public class BalancedBraces {
+
+	public static ArrayList<ArrayList<Character>> printAllBraces(int num) {
+		ArrayList<ArrayList<Character>> resultChar = new ArrayList<ArrayList<Character>>();
+		String parantheses = "{}";
+		List<String> result = new ArrayList<String>();
+		if (num > 0)
+			result.add(parantheses);
+		for (int i = 1; i < num; i++) {
+			List<String> resultCopy = new ArrayList<String>();
+			for (String res : result) {
+				for (int j = 0; j < res.length(); j++) {
+					String str = res.substring(0, j) + parantheses + res.substring(j);
+					if (!resultCopy.contains(str))
+						resultCopy.add(str);
+				}
+				String str = res + parantheses;
+				if (!resultCopy.contains(str))
+					resultCopy.add(str);
+			}
+			result = resultCopy;
+
+		}
+		result.stream().forEach(e -> {
+			System.out.println(e);
+			ArrayList<Character> chars = (ArrayList<Character>) e.chars().mapToObj(a -> (char) a)
+					.collect(Collectors.toList());
+			resultChar.add(chars);
+		});
+		return resultChar;
+	}
+
+	public static void main(String[] args) {
+		ArrayList<ArrayList<Character>> result = new ArrayList<ArrayList<Character>>();
+		result = printAllBraces(3);
+		print(result);
+	}
+
+	static void print(ArrayList<ArrayList<Character>> arr) {
+		for (int i = 0; i < arr.size(); i++) {
+			System.out.println(arr.get(i).toString());
+		}
+	}
+
+}
