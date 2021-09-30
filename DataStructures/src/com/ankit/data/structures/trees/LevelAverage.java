548f398eaba51d40e8aaf5bfe414b246fdf907c8
Ankit Vashishta <ankitvashishta@gmail.com>
2021-09-30 08:26:31 +0800

    Trees : BFS Traversal

---
+++ DataStructures/src/com/ankit/data/structures/trees/LevelAverage.java
@@ -0,0 +1,84 @@
+package com.ankit.data.structures.trees;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Given a binary tree, populate an array to represent the averages of all of
+ * its levels.
+ * 
+ * @author ankit
+ *
+ */
+public class LevelAverage {
+
+	/*
+	 * Time Complexity : O(n) where n is the number of nodes, as we traverse each
+	 * node once.
+	 * 
+	 * Space Complexity : O(n) which is required for the queue. We can have a
+	 * maximum of n/2 nodes at any level, the space required will be hence O(n).
+	 */
+	public static List<Double> findLevelAverages(TreeNode root) {
+		List<Double> result = new ArrayList<>();
+		TreeNode currNode;
+		Queue<TreeNode> queue = new ArrayDeque<>();
+		Queue<TreeNode> queue2 = new ArrayDeque<>();
+		queue.offer(root);
+		while (!queue.isEmpty() || !queue2.isEmpty()) {
+			double sum = 0d;
+			int counter = 0;
+			while (!queue.isEmpty()) {
+				currNode = queue.poll();
+				sum += currNode.val;
+				counter++;
+				if (currNode.left != null)
+					queue2.offer(currNode.left);
+				if (currNode.right != null)
+					queue2.offer(currNode.right);
+			}
+			if (sum != 0)
+				result.add(sum / counter);
+			sum = 0d;
+			counter = 0;
+			while (!queue2.isEmpty()) {
+				currNode = queue2.poll();
+				sum += currNode.val;
+				counter++;
+				if (currNode.left != null)
+					queue.offer(currNode.left);
+				if (currNode.right != null)
+					queue.offer(currNode.right);
+			}
+			if (sum != 0)
+				result.add(sum / counter);
+			sum = 0d;
+			counter = 0;
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(12);
+		root.left = new TreeNode(7);
+		root.right = new TreeNode(1);
+		root.left.left = new TreeNode(9);
+		root.left.right = new TreeNode(2);
+		root.right.left = new TreeNode(10);
+		root.right.right = new TreeNode(5);
+		List<Double> result = LevelAverage.findLevelAverages(root);
+		System.out.print("Level averages are: " + result);
+	}
+}
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
---
+++ DataStructures/src/com/ankit/data/structures/trees/ReverseLevelOrderTraversal.java
@@ -0,0 +1,73 @@
+package com.ankit.data.structures.trees;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Given a binary tree, populate an array to represent its level-by-level
+ * traversal in reverse order, i.e., the lowest level comes first. You should
+ * populate the values of all nodes in each level from left to right in separate
+ * sub-arrays.
+ * 
+ * @author ankit
+ *
+ */
+public class ReverseLevelOrderTraversal {
+	public static List<List<Integer>> traverse(TreeNode root) {
+		LinkedList<List<Integer>> result = new LinkedList<List<Integer>>();
+		TreeNode currNode;
+		Queue<TreeNode> queue = new ArrayDeque<>();
+		Queue<TreeNode> queue2 = new ArrayDeque<>();
+		queue.offer(root);
+		while (!queue.isEmpty() || !queue2.isEmpty()) {
+			List<Integer> levelOrderList = new ArrayList<>();
+			while (!queue.isEmpty()) {
+				currNode = queue.poll();
+				levelOrderList.add(currNode.val);
+				if (currNode.left != null)
+					queue2.offer(currNode.left);
+				if (currNode.right != null)
+					queue2.offer(currNode.right);
+			}
+			if (levelOrderList.size() != 0)
+				result.addFirst(levelOrderList);
+			levelOrderList = new ArrayList<>();
+			while (!queue2.isEmpty()) {
+				currNode = queue2.poll();
+				levelOrderList.add(currNode.val);
+				if (currNode.left != null)
+					queue.offer(currNode.left);
+				if (currNode.right != null)
+					queue.offer(currNode.right);
+			}
+			if (levelOrderList.size() != 0)
+				result.addFirst(levelOrderList);
+		}
+		// TODO: Write your code here
+		return result;
+	}
+
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(12);
+		root.left = new TreeNode(7);
+		root.right = new TreeNode(1);
+		root.left.left = new TreeNode(9);
+		root.right.left = new TreeNode(10);
+		root.right.right = new TreeNode(5);
+		List<List<Integer>> result = ReverseLevelOrderTraversal.traverse(root);
+		System.out.println("Reverse level order traversal: " + result);
+	}
+}
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
