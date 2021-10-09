36f3482af5dfc4c31a5f13186f26dd04646f55bd
Ankit Vashishta <ankitvashishta@gmail.com>
2021-10-09 18:58:18 +0800

    Hashing : Symmetric elements

---
+++ DataStructures/src/com/ankit/data/structures/hashing/Challenge3_CheckSymmetric.java
@@ -0,0 +1,47 @@
+package com.ankit.data.structures.hashing;
+
+import java.util.HashMap;
+
+/**
+ * Find all the symmetric pairs in the given array. By definition, (a,b) and
+ * (c,d) are symmetric pairs if a = d and b = c.
+ * 
+ * Example : arr[][] = [{1, 2}, {3, 4}, {5, 9}, {4, 3}, {9, 5}]
+ * 
+ * Output : {3,4}{5,9}
+ * 
+ * @author ankit
+ *
+ */
+public class Challenge3_CheckSymmetric {
+
+	/*
+	 * Runtime Complexity : O(n), where n is the size of the array. Every row
+	 * contains 2 elements, so we can ignore the constant value.
+	 * 
+	 * Space Complexity : O(n) as the HashMap stores all the pairs.
+	 */
+	public static String findSymmetric(int[][] arr) {
+		String result = "";
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for (int[] rows : arr) {
+			int key = rows[0];
+			int value = rows[1];
+			if (map.containsKey(value) && map.get(value) == key) {
+				result += "{" + value + "," + key + "}";
+			} else {
+				map.put(key, value);
+			}
+		}
+		return result;
+	}
+
+	public static void main(String args[]) {
+
+		int[][] arr = { { 1, 2 }, { 3, 4 }, { 5, 9 }, { 4, 3 }, { 9, 5 } };
+		String symmetric = findSymmetric(arr);
+		System.out.println(symmetric);
+
+	}
+
+}
