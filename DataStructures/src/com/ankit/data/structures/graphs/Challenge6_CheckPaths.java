408e26af3ffb03db723e62749223a8b60d5f159d
Ankit Vashishta <ankitvashishta@gmail.com>
2021-10-11 23:27:27 +0800

    Graphs : Check Path

--- DataStructures/src/com/ankit/data/structures/graphs/Challenge6_CheckPaths.java
+++ DataStructures/src/com/ankit/data/structures/graphs/Challenge6_CheckPaths.java
@@ -32,4 +32,31 @@ public class Challenge6_CheckPaths {
 		}
 		return false;
 	}
+
+	public static void main(String args[]) {
+
+		Graph g1 = new Graph(9);
+		g1.addEdge(0, 2);
+		g1.addEdge(0, 5);
+		g1.addEdge(2, 3);
+		g1.addEdge(2, 4);
+		g1.addEdge(5, 3);
+		g1.addEdge(5, 6);
+		g1.addEdge(3, 6);
+		g1.addEdge(6, 7);
+		g1.addEdge(6, 8);
+		g1.addEdge(6, 4);
+		g1.addEdge(7, 8);
+		g1.printGraph();
+		System.out.println("Path exists: " + checkPath(g1, 0, 7));
+		System.out.println();
+		Graph g2 = new Graph(4);
+		g2.addEdge(0, 1);
+		g2.addEdge(1, 2);
+		g2.addEdge(1, 3);
+		g2.addEdge(2, 3);
+
+		g2.printGraph();
+		System.out.println("Path exists: " + checkPath(g2, 3, 0));
+	}
 }
